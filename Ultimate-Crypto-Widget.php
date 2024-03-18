95bc828bfda4bc68c4db71ba7107ccdbebffbe7a
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-03-18 15:26:31 +0100

    init commit

---
+++ Ultimate-Crypto-Widget.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name:     Ultimate Crypto Widget
+ * Plugin URI:      cyberinfomatic.com
+ * Description:     Crypto Currency Widget on the go!
+ * Author:          Cyber Infomatic
+ * Author URI:      cyberinfomatic.com
+ * Text Domain:     Ultimate-Crypto-Widget
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ultimate_Crypto_Widget
+ */
+
+// Your code starts here.
+
+
